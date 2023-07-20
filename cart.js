6be3f6023cf2df6cb2d2b544befe8a12c384ff13
Rishab Das <das.rishabk@gmail.com>
2023-07-20 19:10:41 -0400

    added const vars

---
+++ cart.js
@@ -0,0 +1,12 @@
+// Define global variable for number of items in the cart
+let quantity = 0;
+
+// Grabbin
+const showQuantity = document.getElementById("show");
+const add = document.getElementById("add");
+const add2 = document.getElementById("add2");
+const add3 = document.getElementById("add3");
+const reset = document.getElementById("reset");
+
+
+
